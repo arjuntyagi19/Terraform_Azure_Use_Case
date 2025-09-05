34f156d75994d9c640246804caeb089493a9c1b1
arjuntyagi19 <arjuntyagi1962003@gmail.com>
2025-09-05 14:39:47 +0530

    Initial commit with main.tf file

---
+++ main.tf
@@ -0,0 +1,39 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "westus2"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "mystoragearjuntyagi" # must be globally unique and 3-24 lowercase letters/numbers
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "TerraformDemo"
+  } 
+}
+
+resource "azurerm_storage_container" "container" {
+  name                  = "tfcontainer"
+  storage_account_name  = azurerm_storage_account.storage.name
+  container_access_type = "private"
+}
+ 
