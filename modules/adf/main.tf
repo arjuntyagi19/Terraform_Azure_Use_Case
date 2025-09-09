2f6a2603a02783ad338028aa38158ca499652e7e
arjuntyagi19 <arjuntyagi1962003@gmail.com>
2025-09-09 14:26:52 +0530

    Keep only main.tf and .gitignore

--- modules/adf/main.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_data_factory" "this" {
-  name                = "adf-weather"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  identity {
-    type = "SystemAssigned"
-  }
-}
